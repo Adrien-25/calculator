b3de4241dfffcd97556c4b0dd9f800dab5ebb1a3
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2024-11-03 01:38:41 +0100

    add expression + design

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -43,24 +43,63 @@ export default function Calculator() {
 
   return (
     <div className="calculator">
-      <div id="display" className="display">{display}</div>
-      <button id="clear" onClick={handleClear}>AC</button>
-      <button id="divide" onClick={() => handleOperatorClick("/")}>/</button>
-      <button id="multiply" onClick={() => handleOperatorClick("*")}>*</button>
-      <button id="seven" onClick={() => handleNumberClick("7")}>7</button>
-      <button id="eight" onClick={() => handleNumberClick("8")}>8</button>
-      <button id="nine" onClick={() => handleNumberClick("9")}>9</button>
-      <button id="subtract" onClick={() => handleOperatorClick("-")}>-</button>
-      <button id="four" onClick={() => handleNumberClick("4")}>4</button>
-      <button id="five" onClick={() => handleNumberClick("5")}>5</button>
-      <button id="six" onClick={() => handleNumberClick("6")}>6</button>
-      <button id="add" onClick={() => handleOperatorClick("+")}>+</button>
-      <button id="one" onClick={() => handleNumberClick("1")}>1</button>
-      <button id="two" onClick={() => handleNumberClick("2")}>2</button>
-      <button id="three" onClick={() => handleNumberClick("3")}>3</button>
-      <button id="equals" onClick={handleEquals}>=</button>
-      <button id="zero" onClick={() => handleNumberClick("0")}>0</button>
-      <button id="decimal" onClick={handleDecimalClick}>.</button>
+      <div id="result-container">
+        <div id="expression">{expression}</div>
+        <div id="display" className="display">
+          {display}
+        </div>
+      </div>
+      <button id="clear" onClick={handleClear}>
+        AC
+      </button>
+      <button id="divide" onClick={() => handleOperatorClick("/")}>
+        /
+      </button>
+      <button id="multiply" onClick={() => handleOperatorClick("*")}>
+        *
+      </button>
+      <button id="seven" onClick={() => handleNumberClick("7")}>
+        7
+      </button>
+      <button id="eight" onClick={() => handleNumberClick("8")}>
+        8
+      </button>
+      <button id="nine" onClick={() => handleNumberClick("9")}>
+        9
+      </button>
+      <button id="subtract" onClick={() => handleOperatorClick("-")}>
+        -
+      </button>
+      <button id="four" onClick={() => handleNumberClick("4")}>
+        4
+      </button>
+      <button id="five" onClick={() => handleNumberClick("5")}>
+        5
+      </button>
+      <button id="six" onClick={() => handleNumberClick("6")}>
+        6
+      </button>
+      <button id="add" onClick={() => handleOperatorClick("+")}>
+        +
+      </button>
+      <button id="one" onClick={() => handleNumberClick("1")}>
+        1
+      </button>
+      <button id="two" onClick={() => handleNumberClick("2")}>
+        2
+      </button>
+      <button id="three" onClick={() => handleNumberClick("3")}>
+        3
+      </button>
+      <button id="equals" onClick={handleEquals}>
+        =
+      </button>
+      <button id="zero" onClick={() => handleNumberClick("0")}>
+        0
+      </button>
+      <button id="decimal" onClick={handleDecimalClick}>
+        .
+      </button>
     </div>
   );
 }
